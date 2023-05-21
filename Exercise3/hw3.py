c8a9d0bef726debca4b2bac85003e20f4a459e10
Ori Blanka <oriblanka305@gmail.com>
2023-05-21 19:29:24 +0300

    Final commit for this EX

--- Exercise3/hw3.py
+++ Exercise3/hw3.py
@@ -47,32 +47,22 @@ def get_color(ray, ambient, obj, hitP, lights, objects, max_depth, level):
     hitP = hitP + 0.01 * normal
     for light in lights:
         light_ray = light.get_light_ray(hitP)
-        I_L = light.get_intensity(hitP)
-        K_D = obj.diffuse
-        K_S = obj.specular
-        N = normal
         L = light.get_light_ray(hitP).direction
         V = normalize(ray.origin - hitP)
-        L_R = reflected(L, N)
+        L_R = reflected(L, normal)
         s_j = 1
         if light_ray.nearest_intersected_object(objects):
             min_distance, nearest_object = light_ray.nearest_intersected_object(objects)
             s_j = calculate_s_j(ray, hitP, min_distance, nearest_object)
-        sigma = sigma + s_j * ((K_D * I_L * np.dot(N, L)) + (K_S * I_L * (np.dot(V, L_R) ** (obj.shininess / 10))))
+        sigma = sigma + s_j * (obj.diffuse * light.get_intensity(hitP) * np.dot(normal, L) + obj.specular * light.get_intensity(hitP) * (np.dot(V, L_R) ** (obj.shininess / 10)))
 
-    I_amb = ambient
-    K_A = obj.ambient
-    color = K_A * I_amb + sigma
-
+    color = obj.ambient * ambient + sigma
     level = level + 1
     if level > max_depth:
         return color
 
-    # reflection
-    # construct new_ray
-    new_direction = reflected(ray.direction, normal)
-    reflected_ray = Ray(hitP, new_direction)
-    # construct new hitP
+    reflected_ray = Ray(hitP, reflected(ray.direction, normal))
+
     if reflected_ray.nearest_intersected_object(objects):
         min_distance, nearest_object = reflected_ray.nearest_intersected_object(objects)
         new_hitP = reflected_ray.origin + min_distance * reflected_ray.direction
@@ -81,15 +71,13 @@ def get_color(ray, ambient, obj, hitP, lights, objects, max_depth, level):
         else:
             normal = nearest_object.normal
         new_hitP = new_hitP + 0.01 * normal
-        # recursion call
+
         color = color + obj.reflection * get_color(reflected_ray, ambient, nearest_object, new_hitP, lights, objects, max_depth, level)
     return color
 
-# TODO: Change function to our version
 def calculate_s_j(ray, hitP, min_distance, nearest_object):
     s_j = 1
-    distance_from_camera = np.linalg.norm(ray.origin - hitP)
-    if nearest_object and distance_from_camera > min_distance:
+    if nearest_object and np.linalg.norm(ray.origin - hitP) > min_distance:
         s_j = 0
 
     return s_j
@@ -98,6 +86,30 @@ def calculate_s_j(ray, hitP, min_distance, nearest_object):
 # TODO
 def your_own_scene():
     camera = np.array([0, 0, 1])
-    lights = []
-    objects = []
+
+    plane = Plane([0, 1, 0], [0, -0.3, 0])
+    plane.set_material([0.2, 0.2, 0.2], [0.2, 0.2, 0.2], [1, 1, 1], 1000, 0.5)
+
+    sphere_a = Sphere([-0.5, 0.2, -1], 0.5)
+    sphere_a.set_material([1, 0, 0], [1, 0, 0], [0.3, 0.3, 0.3], 100, 1)
+
+    sphere_b = Sphere([0.6, 0.5, -0.5], 0.8)
+    sphere_b.set_material([0, 1, 0], [0, 1, 0], [0.3, 0.3, 0.3], 100, 0.2)
+
+    cuboid = Cuboid(
+        [-1, -.75, -2],
+        [-1, -2, -2],
+        [1, -2, -1.5],
+        [1, -.75, -1.5],
+        [2, -2, -2.5],
+        [2, -.75, -2.5]
+    )
+
+    cuboid.set_material([1, 0, 0], [1, 0, 0], [0, 0, 0], 100, 0.5)
+    cuboid.apply_materials_to_faces()
+
+    light1 = PointLight(intensity=np.array([1, 1, 1]), position=np.array([1, 1.5, 1]), kc=0.1, kl=0.1, kq=0.1)
+    light2 = DirectionalLight(intensity=np.array([1, 1, 1]), direction=np.array([1, 1, 1]))
+    lights = [light1, light2]
+    objects = [sphere_a, sphere_b, cuboid, plane]
     return camera, lights, objects
