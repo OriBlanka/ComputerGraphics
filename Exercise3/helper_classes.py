4d5b58e560fd445c86493a54450b0e231c1b08a4
Ori Blanka <oriblanka305@gmail.com>
2023-05-21 16:38:31 +0300

    Fix bugs and adds TODOs

--- Exercise3/helper_classes.py
+++ Exercise3/helper_classes.py
@@ -2,7 +2,6 @@ import math
 
 import numpy as np
 
-EPSILON = 0.00001
 
 # This function gets a vector and returns its normalized form.
 def normalize(vector):
@@ -13,11 +12,8 @@ def normalize(vector):
 # This function returns the vector that reflects from the surface
 def reflected(vector, axis):
     v = np.array([0, 0, 0])
-    v = normalize(vector - 2 * (np.dot(vector, axis)) * axis)
+    v = normalize(vector) - 2 * (np.dot(normalize(vector), axis)) * axis
     return v
-
-def calculate_reflected_ray(vector, intersection_point, normal_vector):
-    return Ray(origin=intersection_point, direction=reflected(vector=vector, axis=normal_vector))
 
 ## Lights
 
@@ -34,12 +30,12 @@ class DirectionalLight(LightSource):
         self.direction = direction
 
     # This function returns the ray that goes from the light source to a point
-    def get_light_ray(self, intersection_point):
+    def get_light_ray(self,intersection_point):
         return Ray(intersection_point, self.direction)
 
     # This function returns the distance from a point to the light source
     def get_distance_from_light(self, intersection):
-        return np.inf
+        pass
 
     # This function returns the light intensity at a point
     def get_intensity(self, intersection):
@@ -59,40 +55,39 @@ class PointLight(LightSource):
         return Ray(intersection, normalize(self.position - intersection))
 
     # This function returns the distance from a point to the light source
-    def get_distance_from_light(self, intersection):
+    def get_distance_from_light(self,intersection):
         return np.linalg.norm(intersection - self.position)
 
     # This function returns the light intensity at a point
     def get_intensity(self, intersection):
         d = self.get_distance_from_light(intersection)
-        return self.intensity / (self.kc + self.kl * d + self.kq * (d ** 2))
+        return self.intensity / (self.kc + self.kl*d + self.kq * (d**2))
 
 
 class SpotLight(LightSource):
     def __init__(self, intensity, position, direction, kc, kl, kq):
         super().__init__(intensity)
         self.position = np.array(position)
-        self.direction = np.array(direction)
+        self.direction = direction
         self.kc = kc
         self.kl = kl
         self.kq = kq
 
+
     # This function returns the ray that goes from the light source to a point
     def get_light_ray(self, intersection):
-        v_direction = normalize(self.position - intersection)
-        return Ray(intersection, v_direction)
+        return Ray(intersection, normalize(self.position - intersection))
 
     def get_distance_from_light(self, intersection):
         return np.linalg.norm(intersection - self.position)
 
     def get_intensity(self, intersection):
         d = self.get_distance_from_light(intersection)
-        f_att = (self.kc + self.kl * d + self.kq * (d ** 2))
-        lr = self.get_light_ray(intersection)
-        return self.intensity * (np.dot(self.direction, lr.direction)) / f_att
+        vec = self.position - intersection
+        f_att = self.kc + self.kl * d + self.kq * (d ** 2)
+        return self.intensity * np.dot(normalize(vec), normalize(self.direction)) / f_att
 
 
-# TOOD: ORI
 class Ray:
     def __init__(self, origin, direction):
         self.origin = origin
@@ -104,7 +99,7 @@ class Ray:
         intersections = None
         nearest_object = None
         min_distance = np.inf
-
+        distance_object_tuple = None
         intersections = {}
 
         for obj in objects:
@@ -112,11 +107,12 @@ class Ray:
                 distance_object_tuple = obj.intersect(self)
                 intersections[distance_object_tuple[1]] = distance_object_tuple[0]
 
+            if distance_object_tuple and distance_object_tuple[0] < min_distance:
+                min_distance, nearest_object = distance_object_tuple
+
         if len(intersections) == 0:
             return None
 
-        if distance_object_tuple and distance_object_tuple[0] < min_distance:
-            min_distance, nearest_object = distance_object_tuple
 
         return min_distance, nearest_object
 
@@ -154,7 +150,6 @@ class Rectangle(Object3D):
         b                 c
         This function gets the vertices and creates a rectangle object
     """
-
     def __init__(self, a, b, c, d):
         """
             ul -> bl -> br -> ur
@@ -171,54 +166,23 @@ class Rectangle(Object3D):
     # Intersect returns both distance and nearest object.
     # Keep track of both.
     def intersect(self, ray: Ray):
-        plane_abcd = Plane(self.normal, self.abcd[0])
-        if plane_abcd.intersect(ray) is not None:
-            t, _ = plane_abcd.intersect(ray)
-            P_t = ray.origin + t * ray.direction
+        n = self.compute_normal()
+        plane = Plane(n, self.abcd[0])
 
-            vectors = self.abcd - P_t
+        if plane.intersect(ray):
+            t, _ = plane.intersect(ray)
+            p = ray.origin + t * ray.direction
 
-            cross_products = np.cross(np.roll(vectors, -1, axis=0), vectors)
-            dot_products = np.dot(cross_products, self.normal)
-            eqs = dot_products.tolist()
-            if all(eq > 0 for eq in eqs):
-                return t, self
-            else:
-                return None
+            for i in range(4):
+                p1 = self.abcd[i] - p
+                p2 = self.abcd[(i + 1) % 4] - p
+                dot_product = np.dot(n, np.cross(p1, p2))
+                if not dot_product > 0:
+                    return None
 
-        else:
-            return None
+            return t, self
+        return None
 
-        #
-        # eq1 = np.dot(np.cross(vectors[0], vectors[1]), self.normal)
-        # eq2 = np.dot(np.cross(vectors[1], vectors[2]), self.normal)
-        # eq3 = np.dot(np.cross(vectors[2], vectors[3]), self.normal)
-        # eq4 = np.dot(np.cross(vectors[3], vectors[0]), self.normal)
-        #
-        # if eq1 > 0 and eq2 > 0 and eq3 > 0 and eq4 > 0:
-        #     return P_t, self
-        # else:
-        #     return None
-
-        # n = self.compute_normal()
-        # X = self.abcd
-        # # check if the noraml dot the direction of the ray is 0
-        # if np.dot(n, ray.direction) == 0:
-        #     return None
-        # plane = Plane(n, self.abcd[0])
-        # if plane.intersect(ray):
-        #     t, _ = plane.intersect(ray)
-        #     p = ray.origin + t * ray.direction
-        #
-        #     for i in range(4):
-        #         p1 = (X[i] - p)
-        #         p2 = (X[(i + 1) % 4] - p)
-        #         if not np.dot(n, np.cross(p1, p2)) > 0:
-        #             return None
-        #
-        #     return t, self
-        # else:
-        #     return None
 
 class Cuboid(Object3D):
     def __init__(self, a, b, c, d, e, f):
@@ -242,11 +206,11 @@ class Cuboid(Object3D):
         h = [e[0] - (c[0] - b[0]), e[1] - (c[1] - b[1]), e[2] - (c[2] - b[2])]
         g = [f[0] - (d[0] - a[0]), f[1] - (d[1] - a[1]), f[2] - (d[2] - a[2])]
         A = Rectangle(a, b, c, d)
-        B = Rectangle(d, c, f, e)
-        C = Rectangle(e, f, g, h)
+        B = Rectangle(d, c, e, f)
+        C = Rectangle(g, h, e, f)
         D = Rectangle(a, b, h, g)
-        E = Rectangle(g, f, a, d)
-        F = Rectangle(b, c, e, h)
+        E = Rectangle(g, a, d, f)
+        F = Rectangle(h, b, c, e)
 
         self.face_list = [A, B, C, D, E, F]
 
@@ -259,23 +223,12 @@ class Cuboid(Object3D):
     def intersect(self, ray: Ray):
         return ray.nearest_intersected_object(self.face_list)
 
-
 class Sphere(Object3D):
     def __init__(self, center, radius: float):
         self.center = center
         self.radius = radius
 
     def intersect(self, ray: Ray):
-        # b = 2 * np.dot(ray.direction, ray.origin - self.center)
-        # c = np.linalg.norm(ray.origin - self.center) ** 2 - (self.radius ** 2)
-        # squerEqueation = b ** 2 - 4 * c
-        # if squerEqueation > 0:
-        #     x1 = (-b + np.sqrt(squerEqueation)) / 2
-        #     x2 = (-b - np.sqrt(squerEqueation)) / 2
-        #     if x1 > 0 and x2 > 0:
-        #         return min(x1, x2), self
-        # return None, None
-
         # Calculate vector from ray origin to sphere center
         ray_to_center = self.center - ray.origin
 
@@ -296,4 +249,3 @@ class Sphere(Object3D):
         else:
             min_t = min(t1, t2)
             return min_t, self
-
--- Exercise3/hw3.py
+++ Exercise3/hw3.py
@@ -25,21 +25,17 @@ def render_scene(camera, ambient, lights, objects, screen_size, max_depth):
             # This is the main loop where each pixel color is computed.
             if ray.nearest_intersected_object(objects):
                 min_distance, nearest_object = ray.nearest_intersected_object(objects)
-                hitP = ray.origin + min_distance * ray.direction
+                p = ray.origin + min_distance * ray.direction
 
                 # getColor
-                color = get_color(ray, ambient, nearest_object, hitP, lights, objects, max_depth, 1)
+                color = get_color(ray, ambient, nearest_object, p, lights, objects, max_depth, 1)
+
             # We clip the values between 0 and 1 so all pixel values will make sense.
             image[i, j] = np.clip(color, 0, 1)
 
     return image
 
-def get_S_L(objects, object_intersection_point, object_intersection_to_light_ray, light):
-    closest_object, minimum_distance_to_object = object_intersection_to_light_ray.nearest_intersected_object(objects)
-
-    return 0 if minimum_distance_to_object < light.get_distance_from_light(object_intersection_point) else 1
-
-
+# TODO: Change function to our version
 def get_color(ray, ambient, obj, hitP, lights, objects, max_depth, level):
     sigma = 0
     normal = None
@@ -89,7 +85,7 @@ def get_color(ray, ambient, obj, hitP, lights, objects, max_depth, level):
         color = color + obj.reflection * get_color(reflected_ray, ambient, nearest_object, new_hitP, lights, objects, max_depth, level)
     return color
 
-
+# TODO: Change function to our version
 def calculate_s_j(ray, hitP, min_distance, nearest_object):
     s_j = 1
     distance_from_camera = np.linalg.norm(ray.origin - hitP)
@@ -98,7 +94,6 @@ def calculate_s_j(ray, hitP, min_distance, nearest_object):
 
     return s_j
 
-
 # Write your own objects and lights
 # TODO
 def your_own_scene():
@@ -106,29 +101,3 @@ def your_own_scene():
     lights = []
     objects = []
     return camera, lights, objects
-
-# from hw3 import *
-#
-# sphere_a = Sphere([-0.5, 0.2, -1],0.5)
-# sphere_a.set_material([1, 0, 0], [1, 0, 0], [0.3, 0.3, 0.3], 100, 1)
-# sphere_b = Sphere([0.8, 0, -0.5],0.3)
-# sphere_b.set_material([0, 1, 0], [0, 1, 0], [0.3, 0.3, 0.3], 100, 0.2)
-# plane = Plane([0,1,0], [0,-0.3,0])
-# plane.set_material([0.2, 0.2, 0.2], [0.2, 0.2, 0.2], [1, 1, 1], 1000, 0.5)
-# background = Plane([0,0,1], [0,0,-3])
-# background.set_material([0.2, 0.2, 0.2], [0.2, 0.2, 0.2], [0.2, 0.2, 0.2], 1000, 0.5)
-#
-#
-# objects = [sphere_a,sphere_b,plane,background]
-#
-# light = PointLight(intensity= np.array([1, 1, 1]),position=np.array([1,1.5,1]),kc=0.1,kl=0.1,kq=0.1)
-#
-# lights = [light]
-#
-# ambient = np.array([0.1,0.2,0.3])
-#
-# camera = np.array([0,0,1])
-#
-# im = render_scene(camera, ambient, lights, objects, (256,256), 3)
-# plt.imshow(im)
-# plt.imsave('scene4.png', im)
